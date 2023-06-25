59a88831e340453f5799b9cbfc12c2b1ae80edb6
Darth-InVader15 <piyush.singh1315@gmail.com>
2023-06-25 18:17:12 +0530

    added logic for signup

--- frontend/src/components/signup.component.js
+++ frontend/src/components/signup.component.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react'
 
 export default class SignUp extends Component {
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    // Get the form values
+    const email = event.target.elements.email.value;
+    const password = event.target.elements.password.value;
+    const instituteName = event.target.elements.instituteName.value;
+    const username = event.target.elements.username.value;
+
+    // Make a POST request to the backend API
+    axios
+      .post('http://localhost:3000/v1/auth/signup', { email, password, instituteName, username })
+      .then((response) => {
+        // Handle the response from the backend
+        if (response.status === 200) {
+          console.log(response);
+          const username = response.data.username;
+          this.props.onLogin({ username }); // Call the onLogin function from props with the username
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   render() {
     return (
       <form>
@@ -12,12 +37,13 @@ export default class SignUp extends Component {
             type="text"
             className="form-control"
             placeholder="choose a cool username"
+            name='username'
           />
         </div>
 
         <div className="mb-3">
           <label>Institute</label>
-          <input type="text" className="form-control" placeholder="instituteName" />
+          <input type="text" className="form-control" placeholder="instituteName" name='instituteName'/>
         </div>
 
         <div className="mb-3">
@@ -26,6 +52,7 @@ export default class SignUp extends Component {
             type="email"
             className="form-control"
             placeholder="Enter email"
+            name='email'
           />
         </div>
 
@@ -35,6 +62,7 @@ export default class SignUp extends Component {
             type="password"
             className="form-control"
             placeholder="Enter password"
+            name='password'
           />
         </div>
 
