6178073d5a0086216f1543577f7afc781531b4dd
Darth-InVader15 <piyush.singh1315@gmail.com>
2023-06-25 12:29:15 +0530

    added frontend login

---
+++ frontend/src/components/signup.component.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+
+export default class SignUp extends Component {
+  render() {
+    return (
+      <form>
+        <h3>Sign Up</h3>
+
+        <div className="mb-3">
+          <label>Username</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="choose a cool username"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label>Institute</label>
+          <input type="text" className="form-control" placeholder="instituteName" />
+        </div>
+
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Sign Up
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Already registered <a href="/sign-in">sign in?</a>
+        </p>
+      </form>
+    )
+  }
+}
